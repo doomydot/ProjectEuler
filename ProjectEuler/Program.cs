d5fe3a23b4dc0f56c0b8ce855da7947403c658f2
Olof Hagström <olof.hagstrom@gmail.com>
2021-12-03 00:08:57 +0100

    Problem 1 Solved

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,1 +1,10 @@
-﻿for (int i = 1000; i > 0; )
+﻿List <int> multipliers = new List<int>();
+for (int i = 0; i < 1000; i++)
+{
+	if (i % 3 == 0 || i % 5 == 0)
+		multipliers.Add(i);
+}
+int sum = 0;
+foreach (int i in multipliers)
+	sum += i;
+Console.WriteLine(sum);
